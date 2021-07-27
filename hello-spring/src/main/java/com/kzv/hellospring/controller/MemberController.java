5845b8ea87c919447b2c1ededb9400fa2a083c8e
kzv <yg9699k@naver.com>
2021-07-27 23:22:19 +0900

    connect h2

--- hello-spring/src/main/java/com/kzv/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/kzv/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.kzv.hellospring.controller;
 
+import com.kzv.hellospring.domain.Member;
 import com.kzv.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -26,7 +32,29 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemeberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    //Controller 에서 생성된 데이터를 담아서 View 로 전달할 때 사용하는 객체.
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+    }
 }
 
 /*
