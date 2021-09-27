6da8792c8460bedd7981c06cc6c73ab07167aa7d
kzv <yg9699k@naver.com>
2021-09-28 00:12:44 +0900

    HelloController dto리턴 테스트

--- springbootJPA_tdd/src/test/java/com/kzv/tdd/springbootjpa_tdd/web/HelloControllerTest.java
+++ springbootJPA_tdd/src/test/java/com/kzv/tdd/springbootjpa_tdd/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.kzv.tdd.springbootjpa_tdd.web;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -23,6 +25,20 @@ public class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                MockMvcRequestBuilders.get("/hello/dto")
+                        .param("name", name)
+                        .param("amount",String.valueOf(amount)))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(name)))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.amount", Matchers.is(amount)));
     }
 
 }
