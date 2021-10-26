3c0c50fbc26d0d7b297b9ddb182ea013de982f18
kzv <yg9699k@naver.com>
2021-10-26 23:31:55 +0900

    boardmapper 수정 중

--- spring-legacy/src/test/java/com/zkv/mvc/mapper/BoardMapperTest.java
+++ spring-legacy/src/test/java/com/zkv/mvc/mapper/BoardMapperTest.java
@@ -1,5 +1,6 @@
 package com.zkv.mvc.mapper;
 
+import com.kzv.mvc.domain.BoardVO;
 import com.kzv.mvc.mapper.BoardMapper;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,5 +21,16 @@ public class BoardMapperTest {
     @Test
     public void testGetList() {
         mapper.getList().forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testInsert(){
+        BoardVO board = new BoardVO();
+        board.setTitle("새로 작성하는 글");
+        board.setContent("새로 작성하는 내용");
+        board.setWriter("새로 작성하는 글쓴이");
+
+        mapper.insert(board);
+        log.info(board);
     }
 }
