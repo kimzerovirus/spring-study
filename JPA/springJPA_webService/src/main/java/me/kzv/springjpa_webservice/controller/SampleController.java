7fc26d1abd73c1b7bca0453db7d5b242f657ddab
kimzerovirus <yg9699k@naver.com>
2021-11-21 11:40:42 +0900

    test

--- JPA/springJPA_webService/src/main/java/me/kzv/springjpa_webservice/controller/SampleController.java
+++ JPA/springJPA_webService/src/main/java/me/kzv/springjpa_webservice/controller/SampleController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SampleController {
 
+    //test
     @GetMapping
     public void ex(){
-
         log.info("ex............");
     }
 }
