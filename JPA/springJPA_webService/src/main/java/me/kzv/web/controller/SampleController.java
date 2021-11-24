698b67747fe6049eaaf47937953820f1375c3fd7
kimzerovirus <yg9699k@naver.com>
2021-11-24 23:37:26 +0900

    link tag

--- JPA/springJPA_webService/src/main/java/me/kzv/web/controller/SampleController.java
+++ JPA/springJPA_webService/src/main/java/me/kzv/web/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,7 +23,7 @@ public class SampleController {
         log.info("ex1........");
     }
 
-    @GetMapping({"/ex2"})
+    @GetMapping({"/ex2","/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
             SampleDTO dto = SampleDTO.builder()
@@ -38,4 +39,27 @@ public class SampleController {
         model.addAttribute("list", list);
     }
 
+    @GetMapping({"/exInline"})
+    public String exInline(RedirectAttributes redirectAttributes){
+
+        log.info("exInline......");
+
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result","success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+        //리다이렉트 할때 result와 dto라는 이름으로 데이터를 전달한다.
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3");
+    }
 }
