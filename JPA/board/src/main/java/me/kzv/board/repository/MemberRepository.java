476e5c1ee747437b8f851267b8ed4f79f624d3b1
kimzerovirus <68390715+kimzerovirus@users.noreply.github.com>
2022-04-25 14:12:30 +0900

    MemberRepository id (Long -> String)

--- JPA/board/src/main/java/me/kzv/board/repository/MemberRepository.java
+++ JPA/board/src/main/java/me/kzv/board/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package me.kzv.board.repository;
 import me.kzv.board.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, String> {
 }
