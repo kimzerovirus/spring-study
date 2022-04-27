4970ed59df3633dd3579498f6c2e4a633df72215
kimzerovirus <68390715+kimzerovirus@users.noreply.github.com>
2022-04-27 15:58:10 +0900

    logout()메서드 설명 추가

--- security/demo/src/main/java/me/kzv/demo/config/SecurityConfig.java
+++ security/demo/src/main/java/me/kzv/demo/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable(); // 일반적으로 세션방식에서는 csrf 토큰이 보안상으로 권장되지만 REST 방식에서는 csrf토큰 값을 항상 알아야되서 불편하므로 사용하지 않는 경우가 많음
         http.logout(); // csrf 토큰을 이용하는 경우 "/logout" url 호출시 로그아웃 버튼이 있는 post방식의 form 제출 화면이 등장한다. disable()상태에서는 get방식으로 로그아웃 가능
-        // http.logout().logoutUrl(); logoutSuccessUrl()등의 커스텀 메서드 존재
+        // http.logout().logoutUrl()로그아웃url logoutSuccessUrl()로그아웃 성공 후 이동url 등의 커스텀 메서드 존재
     }
 
     // TEST
