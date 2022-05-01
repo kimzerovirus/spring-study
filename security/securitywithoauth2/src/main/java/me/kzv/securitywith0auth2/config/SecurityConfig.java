34aa45e5e0d5ce4d9cbb9e672a099ad135612a51
kimzerovirus <yg9699k@naver.com>
2022-05-01 15:42:06 +0900

    시큐리티 소셜 로그인

---
+++ security/securitywithoauth2/src/main/java/me/kzv/securitywith0auth2/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package me.kzv.securitywith0auth2.config;
+
+import me.kzv.securitywith0auth2.security.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration // IoC 빈(bean)을 등록
+@EnableWebSecurity // 필터 체인 관리 시작 어노테이션, 스프링 시큐리티 필터가 스프링 필터체인에 등록된다.
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // 특정 주소 접근시 권한 및 인증을 위한 어노테이션 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                //.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+                //.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and hasRole('ROLE_USER')")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/login")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService)
+        ;
+    }
+}
---
+++ security/securitywithoauth2/src/test/java/me/kzv/securitywith0auth2/SecuritywithjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kzv.securitywith0auth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritywithjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
