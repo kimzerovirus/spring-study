b21ea19b11981a9a5b81a3a888ece13d82bb202d
kimzerovirus <yg9699k@naver.com>
2022-05-15 11:22:11 +0900

    로그인 및 회원가입 로직 처리

--- JPA/board-api/src/main/java/me/kzv/boardapi/web/controller/v1/UserController.java
+++ JPA/board-api/src/main/java/me/kzv/boardapi/web/controller/v1/UserController.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import me.kzv.boardapi.exception.CustomEmailSigninFailedException;
+import me.kzv.boardapi.security.TokenProvider;
 import me.kzv.boardapi.web.domain.User;
 import me.kzv.boardapi.web.dto.CommonResultDto;
 import me.kzv.boardapi.web.dto.ListResultDto;
@@ -11,7 +13,10 @@ import me.kzv.boardapi.web.dto.SingleResultDto;
 import me.kzv.boardapi.exception.CustomUserNotFoundException;
 import me.kzv.boardapi.web.persistence.UserRepository;
 import me.kzv.boardapi.web.service.ResponseService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 @Api(tags = {"1. User"})
 @RequiredArgsConstructor
@@ -21,6 +26,31 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final ResponseService responseService;
+    private final TokenProvider tokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    @ApiOperation(value = "로그인", notes = "이메일 회원 로그인을 한다.")
+    @PostMapping(value = "/signin")
+    public SingleResultDto<String> signin(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
+                                       @ApiParam(value = "비밀번호", required = true) @RequestParam String password) {
+        User user = userRepository.findByUid(id).orElseThrow(CustomEmailSigninFailedException::new);
+        if (!passwordEncoder.matches(password, user.getPassword()))
+            throw new CustomEmailSigninFailedException();
+        return responseService.getSingleResult(tokenProvider.createToken(String.valueOf(user.getIdx()), user.getRoles()));
+    }
+    @ApiOperation(value = "가입", notes = "회원가입을 한다.")
+    @PostMapping(value = "/signup")
+    public CommonResultDto signin(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
+                               @ApiParam(value = "비밀번호", required = true) @RequestParam String password,
+                               @ApiParam(value = "이름", required = true) @RequestParam String name) {
+        userRepository.save(User.builder()
+                .uid(id)
+                .password(passwordEncoder.encode(password))
+                .name(name)
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+        return responseService.getSuccessResult();
+    }
 
     @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회합니다.")
     @GetMapping(value = "/user")
