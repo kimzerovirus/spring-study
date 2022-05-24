84c5d86e1c350a66b0ef9bdf32cd95a154066c6f
kimzerovirus <yg9699k@naver.com>
2022-05-24 23:39:31 +0900

    환경 세팅

---
+++ JPA/shop/src/main/java/me/kzv/shop/domain/MemberRepository.java
@@ -0,0 +1,22 @@
+package me.kzv.shop.domain;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository // 그냥 JPA
+public class MemberRepository {
+
+    @PersistenceContext // 엔티티메니저를 알아서 스프링부트가 주입시켜줌
+    private EntityManager entityManager;
+
+    public Long save(Member member) {
+        entityManager.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+}
