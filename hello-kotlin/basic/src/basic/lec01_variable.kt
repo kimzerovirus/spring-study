2a9f2a0f51832fd080957a200c4b2ba001fae941
kimzerovirus <yg9699k@naver.com>
2022-06-27 23:38:23 +0900

    kotlin basic

---
+++ hello-kotlin/basic/src/basic/lec01_variable.kt
@@ -0,0 +1,35 @@
+package basic
+
+fun main() {
+    // 코틀린은 타입을 무조건 지정해주지 않아도 됨
+    // 명시적으로 작성한다면 타입스크립트처럼 작성
+    // var == let
+    var number1:Long = 10L
+    number1 = 5L
+    println(number1)
+
+    // val == const
+    val number2 = 10L
+    //  number2 = 5L [X]
+
+    // long - Primitive Type
+    // Long - Reference Type <- 연산과정에서 boxing과 unboxing이 일어나 성능 손해 볼 수 있음
+    // 코틀린은 구별하지 않고 Long으로 작성하지만 알아서 Long과 long으로 변환해준다.
+
+    // 코틀린은 기본적으로 변수에 null을 할당할 수 없게 설계되어 있으며,
+    // nullable로 설정하려면 타입을 명시하고 ?를 더해준다.
+    var number3: Long? = 1_000L
+    number3 = null
+
+    // 코틀린에서 객체 인스턴스화를 할때에는 new를 붙이지 않는다.
+    var person = Person("sudo",25)
+}
+
+class Person constructor(val name: String, val age:Int){
+    /*
+        코틀린은 하나의 주(primary)생성자와 다수의 부(secondary)생성자가 있다.
+        주 생성자는 클래스의 헤더로써 클래스의 이름과 동일한 이름을 사용한다.
+        만약, 주 생성자에 어노테이션이나 가시성 변경자가 없다면 생성자 키워드는 생략할 수 있다.
+        class Person constructor(name: String) -> class Person(name: String)
+    */
+}
---
+++ hello-kotlin/basic/src/basic/lec03_type.kt
@@ -0,0 +1,60 @@
+package basic
+
+import java.lang.IllegalArgumentException
+
+fun main() {
+    val number1: Int? = 3
+    val number2: Long = number1?.toLong() ?: 0L // to타입()함수로 타입을 맞춰줘야 된다.
+    val result = number1 ?: 0 / number2.toDouble()
+
+    printAgePossibleNullPerson(null)
+    val person = printAgePossibleNullPerson(Person("HI",18))
+    val name = "asdf"
+//    Java ST
+//    System.out.println(String.format("이름: %s", person.name));
+
+//    if (person is Person) {
+//        println("이름:${person.name}")
+//    }
+
+//    println("이름 : $name")
+
+    // java는 stringbuider로 append 하던것을 이렇게 작성가능
+    val str = """
+        ABC
+        hello
+        world
+        ${name}
+    """.trimIndent()
+
+    // Java ST
+    // String str = "asdf";
+    // char ch = str.charAt(1);
+    println(str[0]) // 요건 자바스크립트랑 비슷한듯a
+    println(str[2])
+}
+
+fun printAgeIfPerson(obj: Any) {
+    if (obj is Person) {
+        val person = obj as Person //생략 가능 (스마트 캐스트 if에서 타입을 걸렀으므로 알아서 형변환해준것)
+        println(person.age)
+    }
+}
+
+fun printAgePossibleNullPerson(obj: Any?) {
+    val person:Person? = obj as? Person
+    println(person?.age)
+}
+
+fun fail(msg: String): Nothing {
+    throw IllegalArgumentException(msg)
+}
+
+/*
+    * 코틀린 만의 특이한 타입 *
+    - Java의 Object = Any
+    - Java의 void = Unit (다른점은 Unit 그 자체로 타입인자로 사용이 가능하다는 점이다.) 또한 함수형 프로그래밍에서 Unit은 단 하나의 인스턴스만을 갖는 타입을 의미하므로 void처럼 없는 존재가 아니라 실재하는 타입이라는 것이다.
+    - 물론 코틀린도 void를 제공한다. Void
+    - Nothing 타입은 함수가 무한루프의 경우나 예외를 던지는 경우와 같이 정상적으로 끝나지 않는 경우 사용한다.
+    - String indexing
+ */
