54a8c4f1011f0814530472d1aca73ee7c99b0998
kimzerovirus <yg9699k@naver.com>
2022-07-05 13:09:05 +0900

    jwt tutorial

---
+++ security/jwt-tutorial/src/main/java/com/example/jwttutorial/controller/HelloController.java
@@ -0,0 +1,53 @@
+package com.example.jwttutorial.controller;
+
+import com.example.jwttutorial.dto.AuthenticationRequestDto;
+import com.example.jwttutorial.dto.AuthenticationResponseDto;
+import com.example.jwttutorial.security.CustomUserDetailsService;
+import com.example.jwttutorial.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class HelloController {
+
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthentcationToken(@RequestBody AuthenticationRequestDto dto) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(dto.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return new ResponseEntity<>(new AuthenticationResponseDto(jwt), HttpStatus.OK);
+
+        /*
+                http://localhost:8080/authenticate -> body { "username":"tester", "password":"1541" } -> return jwt token
+         */
+    }
+
+}
---
+++ security/jwt-tutorial/src/main/java/com/example/jwttutorial/dto/AuthenticationResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.jwttutorial.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponseDto {
+    private String jwt;
+}
