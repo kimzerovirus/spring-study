410ca48d92d15a64014ceacbf7882aaa1aecd102
kimzerovirus <yg9699k@naver.com>
2022-08-13 00:12:20 +0900

    코틀린 제어문을 표현하는 다양한 방법

--- hello-kotlin/basic/src/basic/lec05_.java
+++ hello-kotlin/basic/src/basic/lec05_.java
@@ -21,4 +21,53 @@ public class lec05_ {
 
 //         return score >= 50 ? "P" : "F";
     }
+
+    private void validate(int score) {
+        if (0 <= score && score <= 100) {
+            System.out.println();
+        }
+    }
+
+    private String getGradeWithSwitch(int score) {
+        switch (score / 10) {
+            case 9:
+                return "A";
+            case 8:
+                return "B";
+            case 7:
+                return "C";
+            default:
+                return "D";
+        }
+    }
+
+    private boolean startsWithA(Object obj) {
+        if (obj instanceof String) {
+            return ((String) obj).startsWith("A");
+        } else {
+            return false;
+        }
+    }
+
+    private void judgeNumber(int number) {
+        if (number == 1 || number == 0 || number == -1) {
+            System.out.println("어디서 많이 본듯?");
+        } else {
+            System.out.println( "1, 0, -1이 아닙니다.");
+        }
+    }
+
+    private void judgeNumber2(int number) {
+        if (number == 0){
+            System.out.println("주어진 숫자는 0이다.");
+            return;
+        }
+
+        if (number % 2 == 0) {
+            System.out.println("주어진 숫자는 짝수이다.");
+            return;
+        }
+
+        System.out.println("주어진 숫자는 홀수이다.");
+    }
 }
