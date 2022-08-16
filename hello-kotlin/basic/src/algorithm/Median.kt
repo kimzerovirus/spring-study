14f20d3a6b060a762fddceaaca3ae0ca10d83557
kimzerovirus <yg9699k@naver.com>
2022-08-17 00:54:35 +0900

    최솟값, 중간값, 홀수 구하기

---
+++ hello-kotlin/basic/src/algorithm/Median.kt
@@ -0,0 +1,26 @@
+package algorithm
+
+/** 중위값 구하기 */
+fun median(a: Int, b: Int, c: Int): Int {
+    return if (a >= b) { // a 는 b 보다 크거나 같다
+        if (b >= c) { // a >= b >= c
+            b
+        } else if (a <= c) { // c >= a >= b
+            a
+        } else { // a >= c >= b
+            c
+        }
+    // a 가 b 보다 작은 상황에서 c 의 값?
+    } else if (a > c) { // b > a > c
+        a
+    } else if (b > c) { // a 가 b 보다 작고 c 보다도 작은 경우 b > c > a
+        c
+    } else { // a 가 b 보다 작고 c 는 a 와 b 보다 크다 c > b > a
+        b
+    }
+}
+
+fun main() {
+    val answer = median(2, 2, 1)
+    println(answer)
+}
---
+++ hello-kotlin/basic/src/algorithm/Minimum.kt
@@ -0,0 +1,13 @@
+package algorithm
+
+fun main() {
+    minimum(1,2,3)
+}
+
+// a, b 를 비교 후 그 중 작은 값을 c 와 비교
+fun minimum(a: Int, b: Int, c: Int) {
+    var answer = if (a < b) a else b
+    if (c < answer) answer = c
+
+    println(answer)
+}
