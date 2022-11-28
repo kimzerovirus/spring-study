008124c3140aaa9d14f09517d5c61f6d03c7deec
kimzerovirus <68390715+kimzerovirus@users.noreply.github.com>
2022-11-28 20:28:20 +0900

    Delete toy/simple-board directory

--- toy/simple-board/src/main/java/me/kzv/simpleboard/web/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package me.kzv.simpleboard.web.repository;
-
-import me.kzv.simpleboard.web.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Member findByEmail(String email);
-}
