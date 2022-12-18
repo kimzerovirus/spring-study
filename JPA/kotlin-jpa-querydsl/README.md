a82032661d2ffd2d93ec649a4a6d0a8d53ccdaf9
kimzerovirus <yg9699k@naver.com>
2022-12-18 23:01:11 +0900

    querydsl설정 추가

---
+++ JPA/kotlin-jpa-querydsl/README.md
@@ -0,0 +1,3 @@
+## querydsl
+> 설정 완료 후 `./gradlew build` 명령어를 치면 
+> `build/generated/source/kapt/main` 위치에 q클래스가 생성된다
--- JPA/kotlin-jpa-querydsl/build.gradle.kts
+++ JPA/kotlin-jpa-querydsl/build.gradle.kts
@@ -3,10 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
-    id ("org.jetbrains.kotlin.plugin.allopen") version "1.7.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.7.21"
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21"
     kotlin("plugin.jpa") version "1.7.21"
+    kotlin("kapt") version "1.7.21" //Querydsl
 }
 
 allOpen {
@@ -25,12 +26,18 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Querydsl 추가
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:${dependencyManagement.importedProperties["querydsl.version"]}:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.withType<KotlinCompile> {
