b7e47857861278696c94001e2c74286f8da86b82
kimzerovirus <yg9699k@naver.com>
2023-06-08 00:44:23 +0900

    파일 변경

---
+++ db-skills/jdbc/src/main/java/me/kzv/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package me.kzv.jdbc.connection;
+
+public abstract class ConnectionConst {
+//    public static final String URL = "jdbc:h2:mem:testdb";
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbctest";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "1234";
+}
---
+++ db-skills/jdbc/src/test/java/me/kzv/jdbc/service/MemberServiceV2Test.java
@@ -0,0 +1,69 @@
+package me.kzv.jdbc.service;
+
+import me.kzv.jdbc.domain.Member;
+import me.kzv.jdbc.repository.MemberRepositoryV2;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static me.kzv.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberServiceV2Test {
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV2 memberRepository;
+    private MemberServiceV2 memberService;
+
+    @BeforeEach
+    void before(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    public void 정상이체() throws Exception {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    public void 이체중_예외발생() throws Exception {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEX = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEX);
+
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEX = memberRepository.findById(memberEX.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(10000); // 트랜잭션 롤백 되어 처음 금액 그대로이다.
+        assertThat(findMemberEX.getMoney()).isEqualTo(10000);
+    }
+}
