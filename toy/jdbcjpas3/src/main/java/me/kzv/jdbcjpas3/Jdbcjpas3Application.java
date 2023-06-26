4ebab15713ad9efd65c7f8d7118ae6eed19b7e4f
kimzerovirus <yg9699k@naver.com>
2023-06-26 23:50:55 +0900

    jdbc + jpa

---
+++ toy/jdbcjpas3/src/main/java/me/kzv/jdbcjpas3/Jdbcjpas3Application.java
@@ -0,0 +1,13 @@
+package me.kzv.jdbcjpas3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdbcjpas3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdbcjpas3Application.class, args);
+    }
+
+}
---
+++ toy/jdbcjpas3/src/test/java/me/kzv/jdbcjpas3/Jdbcjpas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kzv.jdbcjpas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdbcjpas3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
