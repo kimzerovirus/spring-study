745ab05cdc22426b54765cce19d0edbb282de28a
kimzerovirus <yg9699k@naver.com>
2023-09-25 23:14:47 +0900

    batch start

---
+++ batch-scheduler/batch-basic/src/main/java/me/kzv/batchbasic/BatchBasicApplication.java
@@ -0,0 +1,13 @@
+package me.kzv.batchbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchBasicApplication.class, args);
+    }
+
+}
---
+++ batch-scheduler/batch-basic/src/test/java/me/kzv/batchbasic/BatchBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kzv.batchbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
