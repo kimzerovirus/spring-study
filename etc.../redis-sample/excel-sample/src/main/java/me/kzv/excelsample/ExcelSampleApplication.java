c923dbdf62f14948bd07ff877ffc61b51663bceb
kimzerovirus <yg9699k@naver.com>
2023-10-07 23:15:18 +0900

    excel sample init

---
+++ etc.../redis-sample/excel-sample/src/main/java/me/kzv/excelsample/ExcelSampleApplication.java
@@ -0,0 +1,13 @@
+package me.kzv.excelsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelSampleApplication.class, args);
+    }
+
+}
---
+++ etc.../redis-sample/excel-sample/src/test/java/me/kzv/excelsample/ExcelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kzv.excelsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
