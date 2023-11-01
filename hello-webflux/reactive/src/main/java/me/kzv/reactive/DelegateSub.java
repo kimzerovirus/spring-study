bd8a7ce70c6073a3f0623ddabc083626aba8c2da
kimzerovirus <yg9699k@naver.com>
2023-11-01 22:11:23 +0900

    reactive day2

---
+++ hello-webflux/reactive/src/main/java/me/kzv/reactive/DelegateSub.java
@@ -0,0 +1,34 @@
+package me.kzv.reactive;
+
+import java.util.concurrent.Flow;
+
+import static java.util.concurrent.Flow.*;
+
+public class DelegateSub implements Subscriber<Integer> {
+    Subscriber sub;
+
+
+    public DelegateSub(Subscriber sub) {
+        this.sub = sub;
+    }
+
+    @Override
+    public void onSubscribe(Subscription s) {
+        sub.onSubscribe(s); // 받아온거 중계만
+    }
+
+    @Override
+    public void onNext(Integer i) {
+        sub.onNext(i);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        sub.onError(t);
+    }
+
+    @Override
+    public void onComplete() {
+        sub.onComplete();
+    }
+}
