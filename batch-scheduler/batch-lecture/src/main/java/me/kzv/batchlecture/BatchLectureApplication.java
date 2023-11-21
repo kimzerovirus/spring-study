19f18a7d3b7d3142e349ea57b8196ad8afd31952
kimzerovirus <yg9699k@naver.com>
2023-11-21 22:07:31 +0900

    spring batch lecture 세팅

---
+++ batch-scheduler/batch-lecture/src/main/java/me/kzv/batchlecture/BatchLectureApplication.java
@@ -0,0 +1,13 @@
+package me.kzv.batchlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchLectureApplication.class, args);
+    }
+
+}
---
+++ batch-scheduler/batch-lecture/src/test/java/me/kzv/batchlecture/BatchLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kzv.batchlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
