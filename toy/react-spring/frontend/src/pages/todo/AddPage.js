11a4b012240f203e0970715f9a482d4426cc937b
kimzerovirus <yg9699k@naver.com>
2023-11-27 22:24:19 +0900

    react spring

---
+++ toy/react-spring/frontend/src/pages/todo/AddPage.js
@@ -0,0 +1,13 @@
+const AddPage = () => {
+
+
+    return ( 
+      <div className="text-3xl font-extrabold">
+        Todo Add Page 
+      </div> 
+    );
+  
+  }
+   
+  export default AddPage;
+  
