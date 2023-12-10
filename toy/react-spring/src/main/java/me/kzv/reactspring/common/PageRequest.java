b4f2b45ac13884b80cc40cc54c83d2ab9d94fe02
kimzerovirus <yg9699k@naver.com>
2023-12-10 22:58:28 +0900

    todo paging service 추가

--- toy/react-spring/src/main/java/me/kzv/reactspring/common/PageRequest.java
+++ toy/react-spring/src/main/java/me/kzv/reactspring/common/PageRequest.java
@@ -7,11 +7,12 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
-@SuperBuilder
+@SuperBuilder // builder 와 달리 상속된 필드까지 빌더를 생성해준다.
 @NoArgsConstructor
 @AllArgsConstructor
 public class PageRequest {
 
+    // 빌더 패턴을 사용하여 인스턴스를 만들 때 특정값으로 초기화하고 싶다면 default 를 사용하면 된다.
     @Builder.Default
     private int page = 1;
 
--- toy/react-spring/src/main/java/me/kzv/reactspring/todo/TodoService.java
+++ toy/react-spring/src/main/java/me/kzv/reactspring/todo/TodoService.java
@@ -1,7 +1,10 @@
 package me.kzv.reactspring.todo;
 
 import lombok.RequiredArgsConstructor;
+import me.kzv.reactspring.common.PageRequest;
+import me.kzv.reactspring.common.PageResponse;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +26,22 @@ public class TodoService {
     public TodoDto get(Long id) {
         Todo todo = todoRepository.findById(id).orElseThrow();
         return modelMapper.map(todo, TodoDto.class);
+    }
+
+    public PageResponse<TodoDto> list(PageRequest dto) {
+        var pageable = org.springframework.data.domain.PageRequest.of(dto.getPage() - 1, dto.getSize(), Sort.by("id").descending());
+        var result = todoRepository.findAll(pageable);
+        var dtoList = result.getContent().stream()
+                .map(todo -> modelMapper.map(todo, TodoDto.class))
+                .toList();
+
+        var totalContent = result.getTotalElements();
+
+        return PageResponse.<TodoDto>withAll()
+                .dtoList(dtoList)
+                .request(dto)
+                .totalCount(totalContent)
+                .build();
     }
 
     @Transactional
