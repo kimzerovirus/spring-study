a51e83a32da5fcfd1bf7a435cd36eff5dce82696
kimzerovirus <yg9699k@naver.com>
2024-02-18 16:11:38 +0900

    cloud sample

---
+++ spring-cloud/spring-gateway/spring-gateway/src/main/java/me/kzv/springgateway/filter/GlobalFilter.java
@@ -0,0 +1,43 @@
+package me.kzv.springgateway.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
+        super(GlobalFilter.Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            var request = exchange.getRequest();
+            var response = exchange.getResponse();
+
+            log.info("Global PRE filter: baseMessage: {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("Global Filter start: request id -> {}", request.getId());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Global POST filter: response status code -> {}", response.getStatusCode());
+                }
+            }));
+        };
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
