d66ca0805ff2c525ba37c14a6cd190bd2038c8ed
kimzerovirus <yg9699k@naver.com>
2024-04-05 17:56:34 +0900

    jooq init

---
+++ spring-jooq/src/main/java/me/kzv/studyjooq/SpringDataJooqApplication.java
@@ -0,0 +1,13 @@
+package me.kzv.studyjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJooqApplication.class, args);
+    }
+
+}
---
+++ spring-jooq/src/test/java/me/kzv/studyjooq/SpringDataJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kzv.studyjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJooqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
