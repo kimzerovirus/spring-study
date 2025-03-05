f69215e3aea0eb592c863f2c4eb184413b576d4a
kimzerovirus <zerovirus96@gmail.com>
2025-03-06 00:13:47 +0900

    flyway 쓸지 liquidbase 쓸지 고민중..

--- multi-tenancy/tenancy-application/build.gradle.kts
+++ multi-tenancy/tenancy-application/build.gradle.kts
@@ -1,10 +1,19 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+plugins {
+    id("org.flywaydb.flyway") version "8.5.13"
+}
+
+apply {
+    plugin("org.flywaydb.flyway")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("org.postgresql:postgresql")
+    implementation ("org.flywaydb:flyway-core")
 }
 
 tasks.withType<BootJar> {
