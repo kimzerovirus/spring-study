f9104a38d3208d0d5270135cda4bb5ebdcf99e92
kimzerovirus <zerovirus96@gmail.com>
2025-04-25 22:08:46 +0900

    상단 디렉토리 java로 변경

--- junit/practice/core/src/test/java/core/disabled/DisabledTest.kt
+++ junit/practice/core/src/test/java/core/disabled/DisabledTest.kt
@@ -1,9 +1,9 @@
-package me.kzv.core.disabled
+package core.disabled
 
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-@Disabled("실행 불가능한 테스트 클래스")
+@Disabled("실행 불가능한 테스트 클래스") // 클래스에서 넣은 value는 ide에서 표시되지 않는듯..
 class DisabledTest {
 
     @Disabled("실행 불가능한 테스트 메서드1")
