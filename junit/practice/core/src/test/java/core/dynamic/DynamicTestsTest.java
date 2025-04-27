2278bdfb2c404c2c234801bcc318468659b40b24
kimzerovirus <zerovirus96@gmail.com>
2025-04-27 17:43:56 +0900

    동적 테스트

---
+++ junit/practice/core/src/test/java/core/dynamic/DynamicTestsTest.java
@@ -0,0 +1,49 @@
+package core.dynamic;
+
+import me.kzv.core.predicate.PositiveNumberPredicate;
+import org.junit.jupiter.api.*;
+
+import java.util.Iterator;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+class DynamicTestsTest {
+
+    private final PositiveNumberPredicate predicate = new PositiveNumberPredicate();
+
+    @BeforeAll
+    static void setUpClass() {
+        System.out.println("@BeforeAll method");
+    }
+
+    @AfterAll
+    static void tearDownClass() {
+        System.out.println("@AfterAll method");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("@BeforeEach method");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("@AfterEach method");
+    }
+
+    @TestFactory
+    Iterator<DynamicTest> positiveNumberPredicateTestCases() {
+
+        // dynamicTest가 실행되는 동안 positiveNumberPredicateTestCases은 한번만 실행되는것이므로
+        // BeforeEach와 AfterEach도 한번만 실행되게 된다.
+
+        return asList(
+                dynamicTest("negative number", () -> assertFalse(predicate.check(-1))),
+                dynamicTest("zero", () -> assertFalse(predicate.check(0))),
+                dynamicTest("positive number", () -> assertTrue(predicate.check(1)))
+        ).iterator();
+    }
+}
