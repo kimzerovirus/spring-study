42ad22ffa4be94cce519bee21e9b39700b781845
kimzerovirus <zerovirus96@gmail.com>
2025-06-21 23:03:16 +0900

    테스트 세팅 및 목표

--- spring-restdocs-tdd/src/test/java/me/kzv/tdd/supports/test/BaseControllerTest.java
+++ spring-restdocs-tdd/src/test/java/me/kzv/tdd/supports/test/BaseControllerTest.java
@@ -3,6 +3,7 @@ package me.kzv.tdd.supports.test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +27,9 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 public class BaseControllerTest {
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected ModelMapper modelMapper;
 
     @Autowired
     protected MockMvc mockMvc;
